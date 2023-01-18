c5e51404598accebd6b359d3dd8c5e6ee3f38bf4
Diego Viana <diegovianaf7@gmail.com>
2023-01-18 16:06:48 -0300

    refactor: normalize the API data to make it easier to manipulate the data

--- src/script.ts
+++ src/script.ts
@@ -1,35 +1,14 @@
 import fetchData from './fetchData.js'
-
-type TransacaoPagamento = 'Boleto' | 'Cartão de Crédito'
-type TransacaoStatus =
-  | 'Paga'
-  | 'Recusada pela operadora de cartão'
-  | 'guardando pagamento'
-  | 'Estornada'
-
-interface TransacaoAPI {
-  Nome: string
-  ID: number
-  Data: string
-  Status: TransacaoStatus
-  Email: string
-  Valor: string
-  ['Valor (R$)']: string
-  ['Forma de Pagamento']: TransacaoPagamento
-  ['Cliente Novo']: number
-}
+import normalizarTransacao from './normalizarTransacao.js'
 
 async function handleData() {
   const data = await fetchData<TransacaoAPI[]>(
     'https://api.origamid.dev/json/transacoes.json'
   )
-  if (data) {
-    data.forEach((item) => {
-      console.log(item['Email'])
-    })
-  }
-  
-  console.log('codigo continuou')
+  if (!data) return
+  const transacoes = data.map(normalizarTransacao)
+
+  console.log(transacoes)
 }
 
 handleData()
